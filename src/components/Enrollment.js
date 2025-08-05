956903a388cdd457482a655b25169dd5d662051b
Lisa Katusiime <lisa1katusiime@gmail.com>
2025-08-05 09:34:24 +0200

    enrollment page

---
+++ src/components/Enrollment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Enrollment = () => {
+  return (
+    <div>
+      <h1>first container</h1>
+      <h1>second container</h1>
+    </div>
+  );
+};
+
+export default Enrollment;
